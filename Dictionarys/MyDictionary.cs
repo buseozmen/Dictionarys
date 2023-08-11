613f16d55349b13e4336777d02a21d3ba05a99f3
buseozmen <79419178+buseozmen@users.noreply.github.com>
2023-08-11 23:23:03 +0300

    Add project files.

---
+++ Dictionarys/MyDictionary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionarys
+{
+    public class MyDictionary<T>
+    {
+        T[] myDictionary;
+        T[] tempArray;
+        public MyDictionary() 
+        {
+            myDictionary = new T[0];
+        }       
+        public void Add(T item)
+        {
+            tempArray = myDictionary;
+            myDictionary = new T[myDictionary.Length+1];
+            for(int i = 0; i < tempArray.Length; i++)
+            {
+                myDictionary[i] = tempArray[i];
+            }
+            myDictionary[myDictionary.Length-1] = item;
+        }
+        
+        public int Count
+        {
+            get {  return myDictionary.Length;  }
+        }
+    }
+}
---
+++ Dictionarys/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+
+namespace Dictionarys
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string>  myDictionary = new MyDictionary<string>( );
+            Console.WriteLine(myDictionary.Count);
+            myDictionary.Add("a");
+            Console.WriteLine(myDictionary.Count);
+            myDictionary.Add("b");
+            Console.WriteLine(myDictionary.Count);
+            myDictionary.Add("c");
+            Console.WriteLine(myDictionary.Count);
+            myDictionary.Add("d");
+            Console.WriteLine(myDictionary.Count);
+            myDictionary.Add("e");
+            Console.WriteLine(myDictionary.Count);
+        }
+    }
+}
